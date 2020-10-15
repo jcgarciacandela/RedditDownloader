c0153c914f4b379d5af67845c3c4ccee8e2e7fe8
Juan Carlos García Candela <jcgarciacandela@gmail.com>
2020-10-15 22:34:55 +0200

    Start withouth delay. Allow 1-pass and exit. Minor changes.

--- src/main/java/com/jcarlos/redditdownloader/services/RedditDownloader.java
+++ src/main/java/com/jcarlos/redditdownloader/services/RedditDownloader.java
@@ -1,12 +1,18 @@
 package com.jcarlos.redditdownloader.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jcarlos.redditdownloader.RedditdownloaderApplication;
 import com.jcarlos.redditdownloader.util.UtilMisc;
 import jdk.jshell.execution.Util;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,12 +21,8 @@ import java.util.PriorityQueue;
 
 @Service
 public class RedditDownloader {
-
-//  @Value("${subreddits}")
-//  private String[] subreddits;
-
-//  @Value("${retardoSubreddits}")
-//  private int retardoSubreddits;
+  @Autowired
+  private ApplicationContext context;
 
   @Value("#{'${subreddits}'.split(',')}")
   private ArrayList<String> subreddits;
@@ -28,10 +30,25 @@ public class RedditDownloader {
   @Value("${ruta}")
   private String rutaBase;
 
+  @Value("#{new Boolean('${onlyOnePass:false}')}")
+  boolean onlyOnePass;
+
+  @PostConstruct
+  public void onStartup() {
+    recuperaPosts();
+  }
+
   @Scheduled(fixedRateString = "${retardoSubreddits}", initialDelay = 0)
   private void recuperaPosts() {
+
+    if (subreddits.size() == 0) {
+      ((ConfigurableApplicationContext) context).close();
+      return;
+    }
     String subredditActual = subreddits.remove(0);
-    subreddits.add(subredditActual);
+    if (!onlyOnePass) {
+      subreddits.add(subredditActual);
+    }
 
     String urlSubreddit = "https://reddit.com/r/" + subredditActual + ".json?limit=100";
     String contenido = UtilMisc.leeURLHTTPS(urlSubreddit);
@@ -56,13 +73,13 @@ public class RedditDownloader {
 //            System.out.println(rutaSalida);
 
             if (url.contains("imgur.com") && url.contains("gifv")) {
-              rutaSalida=rutaSalida.replace(".gifv",".mp4");
+              rutaSalida = rutaSalida.replace(".gifv", ".mp4");
             }
 
             File testRutaSalida = new File(rutaSalida);
             if (!testRutaSalida.exists()) {
-              System.out.println(subredditActual+"\t"+url);
-              if (url.contains("imgur.com")  && url.contains("gifv")) {
+              System.out.println(subredditActual + "\t" + url);
+              if (url.contains("imgur.com") && url.contains("gifv")) {
                 String contenidoImgur = UtilMisc.leeURLHTTPS(url);
 //                System.out.println(contenidoImgur);
                 url = UtilMisc.recuperaRegexpGrupo("(?s)content=\"([^\"]*?\\.mp4)\"", contenidoImgur, 1);
